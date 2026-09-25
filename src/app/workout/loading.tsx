7656a30413c48d42c4e10c08b974d5fcbc4d5d08
Abdullah-Ishaque <faltuboy.321@gmail.com>
2026-09-25 16:29:36 +0600

    Empty workout loading file bug solved

---
+++ src/app/workout/loading.tsx
@@ -0,0 +1,10 @@
+
+const loading = () => {
+    return (
+        <div>
+            <div>Loading <span className="loading loading-spinner loading-xl"></span></div>
+        </div>
+    );
+};
+
+export default loading;
