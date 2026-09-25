161d7b96bd1928dbce928455714fbee97001747a
Abdullah-Ishaque <faltuboy.321@gmail.com>
2026-09-25 16:28:07 +0600

    Loading added

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+
+const loading = () => {
+    return (
+        <div>
+            <div className='items-center text-center Container w-full min-h-screen'>Loading <span className="loading loading-spinner loading-xl"></span></div>
+        </div>
+    );
+};
+
+export default loading;
